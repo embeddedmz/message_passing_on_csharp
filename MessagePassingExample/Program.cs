17a0e9c5557b61b586cf0aa9a97d4f67f560b2fe
Mohamed Amine Mzoughi <mmzoughi@jeanlutzsa.com>
2022-06-09 16:25:25 +0200

    first commit

---
+++ MessagePassingExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace MessagePassingExample
+{
+    class Program
+    {
+        private ResourceManager resource = new ResourceManager(); // should be a singleton/static class...
+        private BlockingCollection<Action<ResourceManager>> queue = new BlockingCollection<Action<ResourceManager>>();
+        public void Start() => Task.Run(ThreadMain);
+        public Task<T> Enqueue<T>(Func<ResourceManager, T> method)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            queue.Add(() => tcs.SetResult(method()));
+            return tcs.Task;
+        }
+        private void ThreadMain()
+        {
+            foreach (var action in queue.GetConsumingEnumerable())
+            {
+                action(resource);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.Start();
+
+            // TODO : spawn threads here that will communicate with the thread managing the resource
+            // using  prog.Enqueue(...)
+            //Thread webServerThread = new Thread(() => ...);
+            //webServerThread.Start();
+            //webServerThread.Join();
+        }
+    }
+}
---
+++ MessagePassingExample/ResourceManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessagePassingExample
+{
+    public class ResourceManager
+    {
+        public void UpdateStatus()
+        {
+            if (true)
+            {
+                ResourceStatusEventArgs args = new ResourceStatusEventArgs();
+                args.SomeValue = 42;
+                args.Timestamp = DateTime.Now;
+                OnResourceStatusUpdated(args);
+            }
+        }
+
+        protected virtual void OnResourceStatusUpdated(ResourceStatusEventArgs e)
+        {
+            EventHandler<ResourceStatusEventArgs> handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<ResourceStatusEventArgs> ThresholdReached;
+
+        public string SendData(string data)
+        {
+            return "Reply received at " + DateTime.Now;
+        }
+    }
+
+    public class ResourceStatusEventArgs : EventArgs
+    {
+        public int SomeValue { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
